ef111893982e0624e167e7a1558061bdf1975129
Bluehippo321 <149278207+Bluehippo321@users.noreply.github.com>
2024-09-30 15:11:08 -0400

    Working more on camera bug

--- Roguelike Game/Assets/Scripts/PlayerLook.cs
+++ Roguelike Game/Assets/Scripts/PlayerLook.cs
@@ -20,8 +20,7 @@ public class MouseLook : MonoBehaviour
         float MouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float MouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        xRotation -= MouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation = Mathf.Clamp(MouseY, -90f, 90f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * MouseX);
